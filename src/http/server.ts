cc87ffd522d98ba236ea928251dd70308c2903bf
Gustavo Campagnin <gcampagnin@gmail.com>
2024-09-12 18:15:05 -0300

    get week summary route

--- src/http/server.ts
+++ src/http/server.ts
@@ -7,15 +7,22 @@ import {
 import { createGoalRoute } from './routes/create-goal'
 import { createCompletionRoute } from './routes/create-completion'
 import { getPendingGoalsRoute } from './routes/get-pending-goals'
+import { getWeekSummaryRoute } from './routes/get-week-summary'
+import fastifyCors from '@fastify/cors'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
+app.register(fastifyCors, {
+  origin: '*',
+})
+
 app.register(createGoalRoute)
 app.register(createCompletionRoute)
 app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app
   .listen({
